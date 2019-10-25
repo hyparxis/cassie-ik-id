6b4d736f9611b804496595349699b987be6b63d1
Pedro Morais <autranemorais@gmail.com>
2019-10-24 22:42:52 -0700

    Update travis

--- ci/install-dependencies.sh
+++ ci/install-dependencies.sh
@@ -1,5 +1,7 @@
 BAZEL_VERSION=1.1.0
 
+sudp apt-update
+apt-get install libmesa-dev
 sudo apt-get install pkg-config zip g++ zlib1g-dev unzip python3
 wget https://github.com/bazelbuild/bazel/releases/download/$BAZEL_VERSION/bazel-$BAZEL_VERSION-installer-linux-x86_64.sh
 chmod +x bazel-$BAZEL_VERSION-installer-linux-x86_64.sh
